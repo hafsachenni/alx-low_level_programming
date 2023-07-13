9b82c58e773a8444bc1b9324021c03fbd5593511
hafsa chenni <hafsachenni77@gmail.com>
2023-07-13 12:12:15 -0700

    me

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,9 +8,9 @@
  * return: always 0 (success)
 */
 
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
