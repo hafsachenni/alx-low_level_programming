d690ab29be30c2e5e5470ee89124c09b8c542853
hafsa chenni <hafsachenni77@gmail.com>
2023-07-13 10:47:44 -0700

    me

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C programm that that prints with puts function
+ *
+ * return (0)
+ */
 int main (void){
 	puts ("/"Programming is like building a multilingual puzzle");
 	return0;
