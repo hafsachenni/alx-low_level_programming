6867c4a94ee0fd8885d57153ad72ff6203e4892f
hafsa chenni <hafsachenni77@gmail.com>
2023-07-13 11:22:14 -0700

    me

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program taht prints with printf function.
+ *
+ *Return: (0) always success
+*/
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art. \n");
+	return (0);
+}
 
