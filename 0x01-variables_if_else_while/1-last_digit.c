7221960e5907c41ee43bee929c5e349fbf563008
hafsa chenni <hafsachenni77@gmail.com>
2023-07-14 11:37:24 -0700

    me

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
- * Main - entry point
+ * main - entry point
  *
  * Description: print the value of n status:
- *              greater tahn, is zero and is not less than 6.
+ *              greater than, is zero and is not less than 6.
  *
  * Return: (0)
 */
