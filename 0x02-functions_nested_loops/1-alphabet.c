0a29cb0987def141a579639ede523f0de5358624
hafsa chenni <hafsachenni77@gmail.com>
2023-07-18 06:51:31 -0700

    me

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: function that prints the alphabet
+ *
+ * Return: always 0
+*/
+
+void print_alphabet(void);
+{
+	int o;
+
+	for (o = 97; o <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
