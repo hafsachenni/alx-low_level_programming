70f0cc8d162593b36301ad0043e0513599febc82
hafsa chenni <hafsachenni77@gmail.com>
2023-07-18 07:39:00 -0700

    me

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,14 +6,12 @@
  * Return: always 0
 */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{
 		_putchar(ch);
-	}
-	_putchar('\n');
+		_putchar('\n');
 	return (0);
 }
