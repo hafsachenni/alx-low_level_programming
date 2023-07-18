223fda33f90b90ba51e72f50760282121cecba1f
hafsa chenni <hafsachenni77@gmail.com>
2023-07-18 10:09:52 -0700

    me

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,16 +11,17 @@
 void print_alphabet_x10(void);
 {
 
-char c;
+char ch;
 int i = 0;
-
 while (i <= 0)
 {
-for (c = 'a'; c <= 'z'; c++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
-	_putchar(c);
-
-	_putchar('\n');
+	_putchar(ch);
+}
+_putchar('\n');
 i++;
 }
+
+
 }
