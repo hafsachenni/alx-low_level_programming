6a1902febcf4f3d81d86b06225171537dd26b234
hafsa chenni <hafsachenni77@gmail.com>
2023-07-18 10:22:51 -0700

    me

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,13 @@
  * Return:void
 */
 
-void print_alphabet_x10(void);
-
+void print_alphabet_x10(void)
 {
 
 char ch;
-int i = 0;
-
-while (i <= 0)
+int i;
+I = 0;
+while (i < 10)
 {
 for (ch = 'a'; ch <= 'z'; ch++)
 {
