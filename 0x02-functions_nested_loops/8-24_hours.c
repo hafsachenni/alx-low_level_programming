8b53cdd78d00a2dd96b2af801312972ea65943dc
hafsa chenni <hafsachenni77@gmail.com>
2023-07-19 10:39:12 -0700

    me

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
-  * jack_bauer - prints every min of the day of Jack Bauer
-  *
-  * Starting from 00:00 to 23:59
-  *
-  * Return: ...
-  */
+ * jack_bauer - prints every min of the day of Jack Bauer
+ *
+ * Starting from 00:00 to 23:59
+ *
+ */
 
 void jack_bauer(void)
 {
@@ -19,7 +18,7 @@ void jack_bauer(void)
 		j = 0;
 		while (j < 60)
 		{
-			_putchar((9i / 10) + '0');
+			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
 			_putchar((j / 10) + '0');
@@ -30,4 +29,3 @@ void jack_bauer(void)
 		i++;
 	}
 }
-
