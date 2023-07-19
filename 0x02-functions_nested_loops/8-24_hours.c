7a8dc766094d7f9238da4f25a27ef02baf0fc339
hafsa chenni <hafsachenni77@gmail.com>
2023-07-19 10:01:36 -0700

    me

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,14 +19,15 @@ void jack_bauer(void)
 		j = 0;
 		while (j < 60)
 		{
-			-putchar((i / 10) + '0');
+			_putchar((9i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
 			j++;
-			}
-			i++;
 		}
+		i++;
 	}
+}
+
