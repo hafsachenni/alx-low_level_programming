ff53aa0a79f1142a910c285bc107fdc811bc1395
hafsa chenni <hafsachenni77@gmail.com>
2023-07-19 11:06:31 -0700

    me

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,30 +3,29 @@
 
 /**
  * print_to_98 - prints numbers to 98
- * @n: starting integer
  *
- * Description: prints all natural number from n - 98
+ * @n: number nput
  *
- * Return: void
+ * Return: Always 0
  */
 
 void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while (n < 98)
-		{
-			printf("%d, ", n);
-			n++;
-		}
+        while (n < 98)
+	{
+	printf("%d, ", n);
+	n++;
+	}
 	}
 	else if (n > 98)
 	{
-		 while (n > 98)
-		 {
-		 printf("%d, ", n);
-		 n--;
-		 }
-		 }
-		 printf("98\n");
-		 }
+	while (n > 98)
+	{
+	printf("%d, ", n);
+	n--;
+	}
+	}
+	 printf("98\n");
+}
