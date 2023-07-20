dc1f80d56e232b21ea5f0247eb819fc765aba715
hafsa chenni <hafsachenni77@gmail.com>
2023-07-20 07:45:28 -0700

    me

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,9 +10,10 @@ void print_numbers(void)
 {
 	int c;
 
-	for (c = 48; c < 58; c++)
+	for (c = '48'; c <= '58'; c++)
 	{
 		_putchar(c);
+		_putchar(' ');
 	}
-	        _putchar('\n');
+	_putchar('\n');
 }
