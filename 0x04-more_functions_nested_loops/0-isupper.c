b05962b8e6385690668816f9e9364e1ae0bdabad
hafsa chenni <hafsachenni77@gmail.com>
2023-07-20 06:09:47 -0700

    me

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,11 +6,11 @@
  * @c: char to check
  *
  * Return: 1 if c is uppercase, 0 otherwise
- */
+*/
 
 int _isupper(int c)
 {
-	for (c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
