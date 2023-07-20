e6d154e0917861aa9bb7f13f8ae6d19471325b97
hafsa chenni <hafsachenni77@gmail.com>
2023-07-20 06:03:49 -0700

    me

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,18 +2,15 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: char to print
+ * @c: char to check
+ *
  * Return: 1 if c is uppercase, 0 otherwise
  */
 
 int _isupper(int c)
 {
 	for (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
