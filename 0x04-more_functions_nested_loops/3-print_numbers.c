42a4216b53789c55609e2194aa52c8c7ce484154
hafsa chenni <hafsachenni77@gmail.com>
2023-07-20 07:20:38 -0700

    me

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9 followed by a new line
+ * print_numbers - prints numbers from 0 to 9 only using putchar twice
  *
  * Return: always 0
 */
