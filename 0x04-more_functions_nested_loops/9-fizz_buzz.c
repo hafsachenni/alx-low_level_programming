11af7bfe1d3dce21c9c0bcd4967283f151fb5b33
hafsa chenni <hafsachenni77@gmail.com>
2023-07-21 06:53:05 -0700

    me

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,23 @@ int main(void)
 
 	while (a < 100)
 	{
-		if ((a % 3) == 0 && (a % 5) != 0)
-		{
-			printf("Fizz ");
-		}
-		else if ((a % 5) == 0 && (a % 3) == 0)
-		{
-			printf("Buzz ");
-		}
-		else if ((a % 5) == 0 && (a % 3) == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else
-		{
-			printf("%d ", a);
-		}
-		a++;
+	if ((a % 3) == 0 && (a % 5) != 0)
+	{
+	printf("Fizz ");
+	}
+	else if ((a % 5) == 0 && (a % 3) == 0)
+	{
+	printf("Buzz ");
+	}
+	else if ((a % 5) == 0 && (a % 3) == 0)
+	{
+	printf("FizzBuzz ");
+	}
+	else
+	{
+	printf("%d ", a);
+	}
+	a++;
 	}
 	printf("Buzz\n");
 	return (0);
