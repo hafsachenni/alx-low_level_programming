e625b246963d957f29e91f6bdf37c0231534da55
hafsa chenni <hafsachenni77@gmail.com>
2023-07-21 07:47:01 -0700

    me

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,7 @@
 /**
  * main - prints the numbers from 0 to 100
  *
- * Return: Always 0
+ * Return: always 0
  */
 
 int main(void)
@@ -16,7 +16,7 @@ int main(void)
 	{
 	printf("Fizz ");
 	}
-	else if ((a % 5) == 0 && (a % 3) == 0)
+	else if ((a % 5) == 0 && (a % 3) != 0)
 	{
 	printf("Buzz ");
 	}
