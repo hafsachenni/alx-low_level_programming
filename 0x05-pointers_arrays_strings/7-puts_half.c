32d88bc169336c073ce544570ad7a3a36a83780c
hafsa chenni <hafsachenni77@gmail.com>
2023-07-24 14:00:11 -0700

    betty is my enemy

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,14 +12,14 @@ void puts_half(char *str)
 
 	b = 0;
 
-		for(a = 0; str[a]!= '\0'; a++)
+		for (a = 0; str[a] != '\0'; a++)
 			b++;
 	n = (b / 2);
 
 	if ((b % 2) == 1)
-		n= ((b + 1) / 2);
+		n = ((b + 1) / 2);
 
-	for (a = n; str[a]!= '\0'; a++)
+	for (a = n; str[a] != '\0'; a++)
 		_putchar(str[a]);
 	_putchar('\n');
 }
