8b9ca2576d342d98288994edf7b4ccb674a53c3d
hafsa chenni <hafsachenni77@gmail.com>
2023-07-24 12:40:22 -0700

    me

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,15 +1,7 @@
-#ifndef MAINH.H
-#define MAINH.H
-
-#include <stdio.h>
-#include <unistd.h>
-
 int _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-void rev_string(char *s);
-
-#endif 
+void rev_string(char *s); 
