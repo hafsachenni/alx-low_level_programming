c1ed05685ee64b7ecbf7e78e570892bc73e182e1
hafsa chenni <hafsachenni77@gmail.com>
2023-07-24 12:21:36 -0700

    me

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes apointer to an int as parameter
+ * and updates the value it pointsto 98
+ * @n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
