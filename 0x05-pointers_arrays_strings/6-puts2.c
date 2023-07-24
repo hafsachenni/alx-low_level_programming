e2ee6ccb72bd30724129f5021a0841f4aaaa64ca
hafsa chenni <hafsachenni77@gmail.com>
2023-07-24 13:39:23 -0700

    me

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every char of a string
+ * starting with the first one
+ * @str : input
+ * Return: print
+ */
+
+void puts2(char *str)
+{
+	int b =0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		b++;
+	}
+	t = b - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
+	}
+	_putchar('\n');
+}
