5fda92fc1cb8e12197cbdd58e05cd58a8523c3b7
hafsa chenni <hafsachenni77@gmail.com>
2023-07-27 09:53:25 -0700

    me

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares 2 strings
+ * @1: first char
+ * @2: second char
+ * Return: if char *s1 == char *s2 return 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int char *s1, char *s2
+		if *s1 == *s2
+			printf (*s1 == *s2)
+}
