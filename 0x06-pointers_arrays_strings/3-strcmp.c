2ac979dab005384859138dcb625b839bc420d21a
hafsa chenni <hafsachenni77@gmail.com>
2023-07-27 09:56:42 -0700

    me

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,14 +2,17 @@
 
 /**
  * _strcmp - function that compares 2 strings
- * @1: first char
- * @2: second char
+ * @s1: first char
+ * @s2: second char
  * Return: if char *s1 == char *s2 return 0
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int char *s1, char *s2
-		if *s1 == *s2
-			printf (*s1 == *s2)
+	while (*s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
