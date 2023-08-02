22b54711b246619a13506369bbd2e0442a3fc75c
hafsa chenni <hafsachenni77@gmail.com>
2023-08-02 05:40:07 -0700

    me

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by new line
+ * @s: input
+ * Return: always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	char s[] = "abcdefghijkl";
+
+	printf("s is :%s\n", s);
+	return (0);
+}
