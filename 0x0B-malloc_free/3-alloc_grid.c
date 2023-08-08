9a08304e5f6abc9db60902a33760f514b1fce1cb
hafsa chenni <hafsachenni77@gmail.com>
2023-08-08 07:21:46 -0700

    me

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,13 +10,13 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **mee;
+	int **mee = NULL;
 	int x, y;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	mee == malloc(sizeof(int *) * height);
+	mee = malloc(sizeof(int *) * height);
 
 	if (mee == NULL)
 		return (NULL);
