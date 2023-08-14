7a6ab5cdafccb88bcb2f885630550e793614de11
hafsa chenni <hafsachenni77@gmail.com>
2023-08-14 03:52:43 -0700

    me

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dof struct
+ * @name: dogs name
+ * @age: his age
+ * @owner: his owners name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
