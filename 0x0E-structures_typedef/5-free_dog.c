63e414503dda3a35a51692726a03dca7d7a2d049
hafsa chenni <hafsachenni77@gmail.com>
2023-08-15 05:55:03 -0700

    me

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * free_dog - function that frees a dog
+ * @d: dog to free
  */
-int main(void)
-{
-    dog_t *my_dog;
 
-    my_dog = new_dog("Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-    free_dog(my_dog);
-    return (0);
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
