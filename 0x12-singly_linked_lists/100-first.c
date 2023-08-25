36b848d052568ccde4dd9d2a03a94b5f74efd8ca
hafsa chenni <hafsachenni77@gmail.com>
2023-08-25 03:30:30 -0700

    me

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print - prints bfr main
+ * Return: void
+ */
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
