b39de4894bccab8db479efee12b57344ac3a2a59
hafsa chenni <hafsachenni77@gmail.com>
2023-08-30 08:49:25 -0700

    kdf

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index
+ * @head: head
+ * @index: leader
+ * Return: value
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *t = *head;
+	listint_t *prev = NULL;
+	unsigned int count = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (!index)
+	{
+		*head =(*head)->next;
+		free(t);
+		return (1);
+	}
+	while (t != NULL && count < index)
+	{
+		prev = t;
+		t = t->next;
+		count++;
+	}
+	if (t == NULL)
+		return (-1);
+
+	prev->next = t->next;
+	free(t);
+
+	return (1);
+}
