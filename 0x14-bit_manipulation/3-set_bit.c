28e49fffc35770cfc767bf71ce059f0f80705fcc
hafsa chenni <hafsachenni77@gmail.com>
2023-08-31 11:05:18 -0700

    m

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - function that sets the value of a bit
+ * @n: pointer
+ * @index: index
+ * Return: 1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (INDEX > 63)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+	return (1);
+}
