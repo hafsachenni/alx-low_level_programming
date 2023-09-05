ff2b675ed33f7c73034cc7eb18e5eeeb1b46cd99
hafsa chenni <hafsachenni77@gmail.com>
2023-09-05 03:55:44 -0700

    do

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_h
+#ifndef MAIN_I
+#define MAIN_I
 
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-ssize_t read_textfile(const char *filename, size_t letters)
-int create_file(const char *filename, char *text_content)
-int append_text_to_file(const char *filename, char *text_content)
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
