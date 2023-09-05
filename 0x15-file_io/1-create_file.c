54143c2bf4c955ecf9d45a5b0bb585a14a492001
hafsa chenni <hafsachenni77@gmail.com>
2023-09-05 03:59:56 -0700

    dk

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fp = open(filename, 0_CREAT | 0_WRONLY | 0_TRUNC, 0600);
+	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fp == -1)
 		return (-1);
 
