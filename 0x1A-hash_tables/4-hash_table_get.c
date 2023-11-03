1179a03556290b143071bc1dc9cd25d486a72708
hafsachenni <hafsachenni77@gmail.com>
2023-11-03 05:42:44 -0700

    think of it as a codes GPS, easier that way

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function that retrieves a value associated with a key
+ * @ht: hash table
+ * @key: key
+ * Return: the value or null if key not found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	int idx;
+	hash_node_t *tmp;
+
+	if (!ht || !key || !strlen(key) || !ht->array || !ht->size)
+		return (NULL);
+
+	idx = key_index((unsigned char *)key, ht->size);
+
+	tmp = ht->array[idx];
+	if (tmp == NULL)
+		return (NULL);
+
+	for (; tmp != NULL; tmp = tmp->next)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+	}
+
+	return (NULL);
+}
